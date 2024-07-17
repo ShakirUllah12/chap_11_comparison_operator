211f232637652d230942023602e50deba6a784c2
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-17 16:27:38 +0500

    chap 11 done

---
+++ script.js
@@ -0,0 +1,116 @@
+// chapter no 11 (comparision operator)
+
+double equal ==
+console.log(1 == 1);
+console.log(2 == "1");
+console.log(1 == 3);
+console.log(2 == 3);
+console.log(1 == 2);
+console.log(3 == 1);
+console.log(1 == 3);
+console.log(2 == 1);
+
+triple equaal ===
+
+console.log(1 === 1);
+// true
+console.log(1 === 2);
+// false
+console.log(1 === "3");
+// false
+console.log(1 === 1);
+// true
+console.log(1 === "1");
+// false
+console.log(1 === "2"); 
+false
+
+(not!)operator
+console.log(!true);
+// false
+console.log(!false);
+true
+this case if value is true so answer is false if value is false if answaer is true
+
+(!=)
+console.log(1!=1);
+// false 1= 1 
+console.log(1!="1");
+// false 1= to 1
+console.log(1!=2);
+// true yes 1 2 ki barabar nahi hi
+(!==)
+console.log(1!==1);
+// so are equal but chanege false
+console.log(1!=="1");
+// true  
+console.log(1!==2);
+true
+
+(greater than >)
+console.log(1 >2);
+// false
+console.log(2>1);
+// true
+console.log(1 >1);
+// false
+console.log(1>"1");
+// false
+console.log(1 >0);
+// true
+console.log(2>3);
+// false
+console.log(3 >2);
+// true
+console.log(2>1);
+// true
+
+(lesss than<)
+(greater than >)
+console.log(1 <2);
+// true
+console.log(2<1);
+// true
+console.log(1 <1);
+// false
+console.log(1<"1");
+// false
+console.log(1 <0);
+// true
+console.log(2<3);
+// true
+console.log(3 <2);
+// false
+console.log(2<1);
+// true
+
+(greater than or equal )
+
+console.log(1>=1);
+// true
+console.log(2>=3);
+// false
+console.log(15>=15);
+// true
+console.log(1>=0);
+// true
+console.log(1>=5);
+false
+
+(less than or equal <=)
+
+console.log(1<=1);
+// true
+console.log(2<=3);
+// true
+console.log(15<=15);
+// true
+console.log(1<=0);
+//false
+
+
+
+
+
+
+  
